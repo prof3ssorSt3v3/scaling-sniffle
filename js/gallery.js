3a40a0cedcf07053f42dc259b0f6f71a9d40d891
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2024-11-22 17:25:15 -0500

    end of class

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,10 @@
 //DOMContentLoaded or IIFE
-const cacheName = 'IcannotThinkOfANameRightNowImages';
+const cacheName = 'IReallyNeedSomeSleep';
 
 (() => {
   //this IIFE runs when the import() method is called
+  getAllCacheImages();
+  getPosition();
 })();
 
 function getPosition() {
@@ -10,19 +12,45 @@ function getPosition() {
   let options = {
     enableHighAccuracy: true,
     timeout: 15000,
-    maximumAge: 0,
+    maximumAge: 1000 * 60 * 60 * 24,
   };
+  navigator.geolocation.getCurrentPosition(ftw, wtf, options);
 }
 
 function ftw(position) {
   //geolocation success
+  console.log(position);
+  document.querySelector('header h1').textContent = position.coords.latitude + ',' + position.coords.longitude;
 }
 function wtf(err) {
   //geolocation fail
 }
 
-async function getAllCacheImages() {}
+async function getAllCacheImages() {
+  let cache = await caches.open(cacheName);
+  let requests = await cache.keys();
+  //returns an array of Request objects
+  let responses = await Promise.all(requests.map((req) => cache.match(req)));
+  //requests.map() loop through all the request objects and return an array
+  // the function called by map() will return a promise
+  // each promise is created by cache.match()
+  let blobs = await Promise.all(responses.map((resp) => resp.blob()));
+  //responses.map() loop through all the response objects and return an array
+  // the function called by map() will return a promise
+  // each promise is created by response .blob()
+  // Promise.all takes an array of Promises and returns an array of results
+  let urls = blobs.map((blob) => URL.createObjectURL(blob));
+  //synchronous method
+  buildGallery(urls);
+}
 
 function buildGallery(urls) {
   //build the HTML on the page with img tags using urls
+  console.log(urls);
+  document.querySelector('main > section').innerHTML = urls
+    .map((url) => {
+      let name = url.replace(`blob:${location.origin}/`, '');
+      return `<p data-ref="${name}"><img src="${url}" alt="${name}" /></p>`;
+    })
+    .join('');
 }
--- js/main.js
+++ js/main.js
@@ -1,14 +1,56 @@
 document.addEventListener('DOMContentLoaded', init);
-const cacheName = 'IcannotThinkOfANameRightNowImages';
+//or use an IIFE
+const cacheName = 'IReallyNeedSomeSleep';
 
 async function init() {
   //when either HTML file loads...
+  addListeners();
+  switch (document.body.id) {
+    case 'home':
+      //index.html has loaded
+      break;
+    case 'gallery':
+      //gallery.html has loaded
+      // let module = await import('./gallery.js');
+      import('./gallery.js');
+      //load the gallery.js file ONLY if we are on gallery.html
+      break;
+    default:
+    //some other page in the site
+  }
 }
 
 function addListeners() {
-  //add listeners for home page
+  //add listeners for home page and EVERY PAGE
+  let imageForm = document.getElementById('imageForm');
+  if (imageForm) {
+    //make sure imageForm exists on current page
+    imageForm.addEventListener('submit', handleImageSubmit);
+  }
 }
 
 async function handleImageSubmit(ev) {
   ev.preventDefault();
+  //stop the form submitting and reloading the page
+  let cache = await caches.open(cacheName);
+  let imageFile = document.getElementById('images').files[0];
+  console.log(imageFile);
+  let response = new Response(imageFile, {
+    status: 200, // HTTP Status
+    headers: {
+      'content-type': imageFile.type, // Mime Type
+      'content-length': imageFile.size, // size in Bytes
+      'x-original-name': imageFile.name,
+    },
+  });
+  if (!imageFile.type.startsWith('image/')) return;
+  //the mime type must include "image/"
+  let ext = imageFile.type.replace('image/', '');
+  // image/png => png  image/gif => gif   image/jpg => jpg
+  let filename = `/${crypto.randomUUID()}.${ext}`;
+  console.log(filename);
+  let url = new URL(filename, location.origin); // new Request()
+  // console.log(filename);
+  // console.log(url);
+  await cache.put(url, response);
 }
